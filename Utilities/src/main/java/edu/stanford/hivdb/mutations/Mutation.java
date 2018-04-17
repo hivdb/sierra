aedb40d57d542f81084ada28f1a3051248e286b7
Philip Tzou <philiptz@stanford.edu>
2018-04-17 11:42:59 -0700

    Expose `getText()` for GraphQL

--- Utilities/src/main/java/edu/stanford/hivdb/mutations/Mutation.java
+++ Utilities/src/main/java/edu/stanford/hivdb/mutations/Mutation.java
@@ -466,6 +466,11 @@ public class Mutation implements Comparable<Mutation> {
 	public String toString() {
 		return getHumanFormat();
 	}
+	
+	public String getText() {
+		// same as `toString()`, only for GraphQL
+		return getHumanFormat();
+	}
 
 	/**
 	 * This method is just an alias name of getShortHumanFormat
