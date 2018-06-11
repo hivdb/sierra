b892e16ce6a78f38427adc959fd04165e8d65941
Philip Tzou <philiptz@stanford.edu>
2018-06-11 13:17:28 -0700

    Update docker image version

---
+++ scripts/release_stable_version.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -e
+
+cd `dirname $0`/..
+if [ -z "$1" ]; then
+    echo "Usage: $0 <VERSION>" >&2
+    exit 1
+fi
+
+cd docker/sierra; make release
+docker tag hivdb/sierra:latest hivdb/sierra:$1
+docker push hivdb/sierra:$1
