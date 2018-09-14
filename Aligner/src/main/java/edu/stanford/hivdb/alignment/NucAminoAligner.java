5c84e979f038bba516eaab8a552df9f2cc9cdd61
Philip Tzou <philiptz@stanford.edu>
2018-09-13 17:30:36 -0700

    Remove an unused import

--- Aligner/src/main/java/edu/stanford/hivdb/alignment/NucAminoAligner.java
+++ Aligner/src/main/java/edu/stanford/hivdb/alignment/NucAminoAligner.java
@@ -32,7 +32,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
