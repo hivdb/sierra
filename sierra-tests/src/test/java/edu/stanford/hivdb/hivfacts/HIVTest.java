1e4204706713179624df4fd129eeec7b82d58ec2
Kaiming Tao <kaiming.tao@stanford.edu>
2020-01-29 17:31:41 -0800

    Move in Test Cases from DrugClassTest.java

--- sierra-tests/src/test/java/edu/stanford/hivdb/hivfacts/HIVTest.java
+++ sierra-tests/src/test/java/edu/stanford/hivdb/hivfacts/HIVTest.java
@@ -19,15 +19,114 @@
 package edu.stanford.hivdb.hivfacts;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import edu.stanford.hivdb.drugs.Drug;
+
+
 public class HIVTest {
-	
+
 	@Test
 	public void testGetInstance() {
 		HIV hiv1 = HIV.getInstance();
 		HIV hiv2 = HIV.getInstance();
 		assertSame(hiv1, hiv2);
-	}
+    }
 
+    @Test
+    public void testgetDrugClasses() {
+        HIV hiv = HIV.getInstance();
+        assertNotNull(hiv.getDrugClasses());
+    }
+    
+    @Test
+    public void testGetStrain() {
+    	HIV hiv = HIV.getInstance();
+    	assertNotNull(hiv.getStrain("HIV1"));
+    }
+    
+    @Test
+    public void testGetDrugClass() {
+    	HIV hiv = HIV.getInstance();
+    	assertEquals(hiv.getDrugClass("PI").getAbstractGene(), "PR");
+    	assertEquals(hiv.getDrugClass("NRTI").getAbstractGene(), "RT");
+    	assertEquals(hiv.getDrugClass("NNRTI").getAbstractGene(), "RT");
+    	assertEquals(hiv.getDrugClass("INSTI").getAbstractGene(), "IN");
+    	
+    	assertEquals(
+    			hiv.getDrugClass("PI").getFullName(),
+    			"Protease Inhibitor"
+    			
+    			);
+    	assertEquals(
+    			hiv.getDrugClass("NRTI").getFullName(),
+    			"Nucleoside Reverse Transcriptase Inhibitor"
+    			);
+    	assertEquals(
+    			hiv.getDrugClass("NNRTI").getFullName(),
+    			"Non-nucleoside Reverse Transcriptase Inhibitor"
+    			);
+    	assertEquals(
+    			hiv.getDrugClass("INSTI").getFullName(),
+    			"Integrase Strand Transfer Inhibitor"
+    			);  	
+    	
+    }
+    
+    @Test
+    public void testGetDrugClassSynonymMap() {
+    	HIV hiv = HIV.getInstance();
+    	assertEquals(hiv.getDrugClassSynonymMap().get("PI").getName(), "PI");
+    	assertEquals(hiv.getDrugClassSynonymMap().get("NRTI").getName(), "NRTI");
+    	assertEquals(hiv.getDrugClassSynonymMap().get("NNRTI").getName(), "NNRTI");
+    	assertEquals(hiv.getDrugClassSynonymMap().get("INSTI").getName(), "INSTI");
+    	assertEquals(hiv.getDrugClassSynonymMap().get("INI").getName(), "INSTI");
+
+    }
+    
+    @Test
+    public void testGetDrugs() {
+    	HIV hiv = HIV.getInstance();
+    	
+    	List<Drug<HIV>> piExpecteds = new ArrayList<>();
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("ATV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("DRV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("FPV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("IDV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("LPV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("NFV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("SQV"));
+    	piExpecteds.add(hiv.getDrugSynonymMap().get("TPV"));
+    	assertEquals(new ArrayList<Drug<HIV>>(hiv.getDrugClass("PI").getDrugs()), piExpecteds);
+    	
+    	List<Drug<HIV>> nrtiExpecteds = new ArrayList<>();
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("ABC"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("AZT"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("D4T"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("DDI"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("FTC"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("LMV"));
+    	nrtiExpecteds.add(hiv.getDrugSynonymMap().get("TDF"));
+    	assertEquals(new ArrayList<Drug<HIV>>(hiv.getDrugClass("NRTI").getDrugs()), nrtiExpecteds);
+    	
+    	List<Drug<HIV>> nnrtiExpecteds = new ArrayList<>();
+    	nnrtiExpecteds.add(hiv.getDrugSynonymMap().get("DOR"));
+    	nnrtiExpecteds.add(hiv.getDrugSynonymMap().get("EFV"));
+    	nnrtiExpecteds.add(hiv.getDrugSynonymMap().get("ETR"));
+    	nnrtiExpecteds.add(hiv.getDrugSynonymMap().get("NVP"));
+    	nnrtiExpecteds.add(hiv.getDrugSynonymMap().get("RPV"));
+    	assertEquals(new ArrayList<Drug<HIV>>(hiv.getDrugClass("NNRTI").getDrugs()), nnrtiExpecteds);
+    	
+    	List<Drug<HIV>> instiExpecteds = new ArrayList<>();
+
+    	instiExpecteds.add(hiv.getDrugSynonymMap().get("BIC"));
+    	instiExpecteds.add(hiv.getDrugSynonymMap().get("DTG"));
+    	instiExpecteds.add(hiv.getDrugSynonymMap().get("EVG"));
+    	instiExpecteds.add(hiv.getDrugSynonymMap().get("RAL"));
+    	assertEquals(new ArrayList<Drug<HIV>>(hiv.getDrugClass("INSTI").getDrugs()), instiExpecteds);
+    }
 }
