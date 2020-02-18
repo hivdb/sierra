9b9e123277454bcc36ffc7e6fc195ca04698c9a0
Kaiming Tao <kaiming.tao@stanford.edu>
2020-02-18 14:26:46 -0800

    update GenePositionTest.java

--- sierra-tests/src/test/java/edu/stanford/hivdb/mutations/GenePositionTest.java
+++ sierra-tests/src/test/java/edu/stanford/hivdb/mutations/GenePositionTest.java
@@ -28,40 +28,6 @@ public class GenePositionTest {
 	@Rule
 	public ExpectedException expectedEx = ExpectedException.none();
 
-	// Potential issues:
-	// 1. Doesn't throw out of bounds exceptions.
-    @Test
-    public void testGetInstance_int() {
-
-    	GenePosition<HIV> prGP = new GenePosition<HIV>(hiv.getGene("HIV1PR"), MAX_PR_POS);
-    	assertEquals(prGP.getGene(), hiv.getGene("HIV1PR"));
-    	assertEquals(prGP.getPosition(), Integer.valueOf(MAX_PR_POS));
-
-    	GenePosition<HIV> rtGP = new GenePosition<HIV>(hiv.getGene("HIV1RT"), MAX_RT_POS);
-    	assertEquals(rtGP.getGene(), hiv.getGene("HIV1RT"));
-    	assertEquals(rtGP.getPosition(), Integer.valueOf(MAX_RT_POS));
-
-    	GenePosition<HIV> inGP = new GenePosition<HIV>(hiv.getGene("HIV1IN"), MAX_IN_POS);
-    	assertEquals(inGP.getGene(), hiv.getGene("HIV1IN"));
-    	assertEquals(inGP.getPosition(), Integer.valueOf(MAX_IN_POS));
-
-    	GenePosition<HIV> inGPMin = new GenePosition<HIV>(hiv.getGene("HIV1IN"), Integer.MIN_VALUE);
-    	assertEquals(inGPMin.getGene(), hiv.getGene("HIV1IN"));
-    	assertEquals(inGPMin.getPosition(), Integer.valueOf(Integer.MIN_VALUE));
-
-    	GenePosition<HIV> inGPMax = new GenePosition<HIV>(hiv.getGene("HIV1IN"), Integer.MAX_VALUE);
-    	assertEquals(inGPMax.getGene(), hiv.getGene("HIV1IN"));
-    	assertEquals(inGPMax.getPosition(), Integer.valueOf(Integer.MAX_VALUE));
-
-    }
-
-	@Test
-	public void testGetInstance_Integer() {
-		GenePosition<HIV> prGP = new GenePosition<HIV>(hiv.getGene("HIV1PR"), Integer.valueOf(MAX_PR_POS));
-    	assertEquals(prGP.getGene(), hiv.getGene("HIV1PR"));
-    	assertEquals(prGP.getPosition(), Integer.valueOf(MAX_PR_POS));
-	}
-
     @SuppressWarnings("unchecked")
 	@Test
     public void testGetGenePositionsBetween() {
@@ -103,6 +69,66 @@ public class GenePositionTest {
     	assertEquals(100, gps4.size());
 
     }
+   
+	@Test
+	public void testGetGenePositionsBetweenMismatchedStrain() {
+	 	expectedEx.expect(IllegalArgumentException.class);
+	 	expectedEx.expectMessage("Virus strain of `start` and `end` positions must be the same.");
+	 	GenePosition.getGenePositionsBetween(
+	 		new GenePosition<HIV2>(hiv2.getGene("HIV2BPR"), 50),
+	 		new GenePosition<HIV2>(hiv2.getGene("HIV2AIN"), 50)
+	 	);
+	}
+    
+    @Test
+    public void testGetDRGenePositions() {
+    	Set<GenePosition<HIV>> gps1 = GenePosition.getDRGenePositionsBetween(
+    			new GenePosition<HIV>(hiv.getGene("HIV1PR"), 1),
+    			new GenePosition<HIV>(hiv.getGene("HIV1IN"), 288)
+    		);
+    	assertEquals(gps1.size(),88);
+    }
+	
+	
+	// Potential issues:
+	// 1. Doesn't throw out of bounds exceptions.
+    @Test
+    public void testGetInstance_int() {
+
+    	GenePosition<HIV> prGP = new GenePosition<HIV>(hiv.getGene("HIV1PR"), MAX_PR_POS);
+    	assertEquals(prGP.getGene(), hiv.getGene("HIV1PR"));
+    	assertEquals(prGP.getPosition(), Integer.valueOf(MAX_PR_POS));
+
+    	GenePosition<HIV> rtGP = new GenePosition<HIV>(hiv.getGene("HIV1RT"), MAX_RT_POS);
+    	assertEquals(rtGP.getGene(), hiv.getGene("HIV1RT"));
+    	assertEquals(rtGP.getPosition(), Integer.valueOf(MAX_RT_POS));
+
+    	GenePosition<HIV> inGP = new GenePosition<HIV>(hiv.getGene("HIV1IN"), MAX_IN_POS);
+    	assertEquals(inGP.getGene(), hiv.getGene("HIV1IN"));
+    	assertEquals(inGP.getPosition(), Integer.valueOf(MAX_IN_POS));
+
+    	GenePosition<HIV> inGPMin = new GenePosition<HIV>(hiv.getGene("HIV1IN"), Integer.MIN_VALUE);
+    	assertEquals(inGPMin.getGene(), hiv.getGene("HIV1IN"));
+    	assertEquals(inGPMin.getPosition(), Integer.valueOf(Integer.MIN_VALUE));
+
+    	GenePosition<HIV> inGPMax = new GenePosition<HIV>(hiv.getGene("HIV1IN"), Integer.MAX_VALUE);
+    	assertEquals(inGPMax.getGene(), hiv.getGene("HIV1IN"));
+    	assertEquals(inGPMax.getPosition(), Integer.valueOf(Integer.MAX_VALUE));
+
+    }
+
+	@Test
+	public void testGetInstance_Integer() {
+		GenePosition<HIV> prGP = new GenePosition<HIV>(hiv.getGene("HIV1PR"), Integer.valueOf(MAX_PR_POS));
+    	assertEquals(prGP.getGene(), hiv.getGene("HIV1PR"));
+    	assertEquals(prGP.getPosition(), Integer.valueOf(MAX_PR_POS));
+	}
+	
+	@Test
+	public void testGetPosition() {
+		GenePosition<HIV> prGP = new GenePosition<HIV>(hiv.getGene("HIV1PR"), MAX_PR_POS);
+		assertEquals(prGP.getPosition(), Integer.valueOf(99));
+	}
 
     @Test
 	public void testGetPositionInStrain() {
@@ -115,20 +141,71 @@ public class GenePositionTest {
 		assertEquals(659, (int) new GenePosition<HIV2>(hiv2.getGene("HIV2AIN"), 1).getPositionInStrain());
 	}
 
-	 @Test
-	 public void testGetGenePositionsBetweenMismatchedStrain() {
-	 	expectedEx.expect(IllegalArgumentException.class);
-	 	expectedEx.expectMessage("Virus strain of `start` and `end` positions must be the same.");
-	 	GenePosition.getGenePositionsBetween(
-	 		new GenePosition<HIV2>(hiv2.getGene("HIV2BPR"), 50),
-	 		new GenePosition<HIV2>(hiv2.getGene("HIV2AIN"), 50)
-	 	);
-	}
-
 	@Test(expected = NullPointerException.class)
 	public void testCompareToException() {
 		final GenePosition<HIV> gp = new GenePosition<HIV>(hiv.getGene("HIV1PR"), 1);
 		assertEquals(gp.compareTo(null), 1);
+	}
+	
+	@Test
+	public void  testIsDrugResistancePosition() {
+		GenePosition<HIV> gp = new GenePosition<HIV>(hiv.getGene("HIV1PR"), 1);
+		assertFalse(gp.isDrugResistancePosition());
+		
+		gp = new GenePosition<HIV>(hiv.getGene("HIV1RT"), 184);
+		assertTrue(gp.isDrugResistancePosition());
+	}
+	
+	@Test
+	public void testGetGene() {
+		GenePosition<HIV> gp = new GenePosition<HIV>(hiv.getGene("HIV1RT"), 184);
+		
+		assertEquals(hiv.getGene("HIV1RT"), gp.getGene());
+	}
+	
+	@Test
+	public void testGetStrain() {
+		GenePosition<HIV> gp = new GenePosition<HIV>(hiv.getGene("HIV1RT"), 184);
+		assertEquals(hiv.getStrain("HIV1"), gp.getStrain());
+	}
+	
+	@Test
+	public void testGetAbstractGene() {
+		GenePosition<HIV> gp = new GenePosition<HIV>(hiv.getGene("HIV1RT"), 184);
+		assertEquals("RT", gp.getAbstractGene());
+		
+	}
+	
+	@Test
+	@SuppressWarnings("unlikely-arg-type")
+	public void testEquals() {
+		GenePosition<HIV> gpPR5 = new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5);
+		assertFalse(gpPR5.equals(null));
+		assertTrue(gpPR5.equals(gpPR5));
+		assertTrue(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5)));
+		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 6)));
+		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5)));
+		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 6)));
+
+		assertFalse(gpPR5.equals("HIV1PR:5"));
+	}
+	
+	@Test
+	public void testHashCode() {
+		assertEquals(1, new GenePosition<HIV>(hiv.getGene("HIV1PR"), 6).hashCode() -
+                        new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5).hashCode());
+		assertEquals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode(),
+                     new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode());
+		assertNotEquals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5).hashCode(),
+                        new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode());
+
+		Set<Integer> hashCodes = new HashSet<Integer>();
+		for (int pos = 1; pos <= MAX_RT_POS; pos++) {
+			if (pos <= MAX_PR_POS) hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1PR"), pos).hashCode());
+			if (pos <= MAX_IN_POS) hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1IN"), pos).hashCode());
+			hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1RT"), pos).hashCode());
+		}
+		assertEquals(hashCodes.size(), TOTAL_POS);
 	}
 
     @Test
@@ -154,41 +231,8 @@ public class GenePositionTest {
 	}
 
 	@Test
-	@SuppressWarnings("unlikely-arg-type")
-	public void testEquals() {
-		GenePosition<HIV> gpPR5 = new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5);
-		assertFalse(gpPR5.equals(null));
-		assertTrue(gpPR5.equals(gpPR5));
-		assertTrue(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5)));
-		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 6)));
-		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5)));
-		assertFalse(gpPR5.equals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 6)));
-
-		assertFalse(gpPR5.equals("HIV1PR:5"));
-	}
-
-	@Test
 	public void testToString() {
 		assertEquals("HIV1PR:5", new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5).toString());
 	}
-
-	@Test
-	public void testHashCode() {
-		assertEquals(1, new GenePosition<HIV>(hiv.getGene("HIV1PR"), 6).hashCode() -
-                        new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5).hashCode());
-		assertEquals(new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode(),
-                     new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode());
-		assertNotEquals(new GenePosition<HIV>(hiv.getGene("HIV1PR"), 5).hashCode(),
-                        new GenePosition<HIV>(hiv.getGene("HIV1RT"), 5).hashCode());
-
-		Set<Integer> hashCodes = new HashSet<Integer>();
-		for (int pos = 1; pos <= MAX_RT_POS; pos++) {
-			if (pos <= MAX_PR_POS) hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1PR"), pos).hashCode());
-			if (pos <= MAX_IN_POS) hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1IN"), pos).hashCode());
-			hashCodes.add(new GenePosition<HIV>(hiv.getGene("HIV1RT"), pos).hashCode());
-		}
-		assertEquals(hashCodes.size(), TOTAL_POS);
-	}
-
 
 }
