2be66b057e0fab47ccb4229d94c5c3d110cfe843
Kaiming Tao <kaiming.tao@stanford.edu>
2020-02-27 11:27:16 -0800

    update AminoAcidPercentTest.java

--- sierra-tests/src/test/java/edu/stanford/hivdb/mutations/AminoAcidPercentTest.java
+++ sierra-tests/src/test/java/edu/stanford/hivdb/mutations/AminoAcidPercentTest.java
@@ -9,26 +9,86 @@ import edu.stanford.hivdb.mutations.AminoAcidPercent;
 
 public class AminoAcidPercentTest {
 
-	final static HIV hiv = HIV.getInstance();
+	private final static HIV hiv = HIV.getInstance();
+	private static AminoAcidPercent<HIV> aaPcnt;
+	
+	private static void initInstance() {
+		aaPcnt = new AminoAcidPercent<HIV>(
+				hiv.getGene("HIV1PR"),
+				1,
+				'A',
+				2.6901969224147208e-05,
+				4,
+				148688,
+				"PCNT",
+				true
+			);
+	}
+	
+	static {
+		initInstance();
+	}
 
 	@Test
-	public void test() {
-		AminoAcidPercent<HIV> mPR5A = new AminoAcidPercent<HIV>(
-			hiv.getGene("HIV1PR"), 5, 'A',
-			/* percent   = */ 6.7597712493409226e-06,
-			/* count     = */ 1,
-			/* total     = */ 147934,
-			/* reason    = */ "PCNT",
-			/* isUnusual = */ true
-		);
-		assertEquals(hiv.getGene("HIV1PR"), mPR5A.getGene());
-		assertEquals(5, (int) mPR5A.getPosition());
-		assertEquals('A', (char) mPR5A.getAA());
-		assertEquals(6.7597712493409226e-06, mPR5A.getPercent(), 1e-18);
-		assertEquals(1, (int) mPR5A.getCount());
-		assertEquals(147934, (int) mPR5A.getTotal());
-		assertEquals("PCNT", mPR5A.getReason());
-		assertTrue(mPR5A.isUnusual());
+	public void testAminoAcidPercent() {
+
+		assertNotNull(aaPcnt);
+
+	}
+	
+	@Test
+	public void testGetStrain() {
+		assertEquals(aaPcnt.getStrain(), hiv.getStrain("HIV1"));
+	}
+	
+	@Test
+	public void testGetGene() {
+		assertEquals(aaPcnt.getGene(), hiv.getGene("HIV1PR"));
+	}
+	
+	@Test
+	public void testGetAbstractGene() {
+		assertEquals(aaPcnt.getAbstractGene(), "PR");
+	}
+
+	@Test
+	public void testGetPosition() {
+		assertEquals(aaPcnt.getPosition(), Integer.valueOf(1));
+	}
+	
+	@Test
+	public void testGetAA() {
+		assertEquals(aaPcnt.getAA(), Character.valueOf('A'));
+	}
+	
+	@Test
+	public void testGetRefChar() {
+		assertEquals(aaPcnt.getRefChar(), Character.valueOf('P'));
+	}
+	
+	@Test
+	public void testGetMutation() {
+		assertTrue(aaPcnt.getMutation() instanceof AAMutation<?>);
+	}
+	
+	@Test
+	public void testGetPercent() {
+		assertEquals(aaPcnt.getPercent(), 2.6901969224147208e-05, 1e-05);
+	}
+	
+	@Test
+	public void testGetCount() {
+		assertEquals(aaPcnt.getCount(), Integer.valueOf(4));
+	}
+	
+	@Test
+	public void testGetTotal() {
+		assertEquals(aaPcnt.getTotal(), Integer.valueOf(148688));
+	}
+	
+	@Test
+	public void testGetReason() {
+		assertEquals(aaPcnt.getReason(), "PCNT");
 	}
 
 }
